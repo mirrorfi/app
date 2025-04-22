439d6230b2ae84faec52b695beaa99a0adb220c7
Skooy <104994184+Skooyo@users.noreply.github.com>
2025-04-22 15:26:51 +0800

    Added db connection, requires .env.local

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import { getAllTest } from "@/lib/database/db_actions/test-actions";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.log(getAllTest());
+  }, [])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
